eb92e1153c358a590976dc9df7173843f055a874
Ugorskaya <z_942011@mail.ru>
2015-12-15 22:30:23 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,9 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner sc = new Scanner(System.in);
+		
+		
+			//для дробей
 			Fraction a=new Fraction();
 			Fraction b=new Fraction();
-			
+
 			a.n=1; a.d=5;
 			b.n=2; b.d=3;
 			System.out.println(a);
@@ -22,7 +26,24 @@ public class Main {
 			System.out.println(a+" / "+b+"="+a.del(b));
 			System.out.println(a+" * "+b+"="+a.umn(b));
 			
-
+			//для фигур
+			System.out.println();
+			System.out.println("Введите радиус круга: ");
+			figure c= new circle(0, 0, sc.nextInt());
+			System.out.println("Площадь = "+c.area()+"; Периметр = "+c.perimeter());
+			System.out.println();
+			
+			System.out.println();
+			System.out.println("Введите длину сторон прямоугольника: ");
+			figure pr= new rect(0, 0, sc.nextInt(), sc.nextInt());
+			System.out.println("Площадь = "+pr.area()+"; Периметр = "+pr.perimeter());
+			System.out.println();
+			
+			System.out.println();
+			System.out.println("Введите длину стороны квадрата: ");
+			figure kv= new square(0, 0, sc.nextInt());
+			System.out.println("Площадь = "+kv.area()+"; Периметр = "+kv.perimeter());
+			System.out.println();
 	}
 
 }
